351586ba2c3524eac30bd8f6efc4d24c567e713d
namn44241 <nguyenvandoaaaaa@gmail.com>
2024-12-18 14:08:52 +0700

    Update ProductList.js

--- react/client/src/components/menu/ProductList.js
+++ react/client/src/components/menu/ProductList.js
@@ -157,7 +157,7 @@ function ProductList({ filter, viewMode }) {
                   <div className="col-md-3">
                     <Link to={`/product/${product.id}`}>
                       <img 
-                        src={`http://localhost:5001/uploads/products/${product.image_name}`} 
+                        src={`https://foodee.namtech.me/uploads/products/${product.image_name}`} 
                         alt={product.name}
                         className="img-fluid rounded"
                         onError={(e) => {
