e5520b22934a3a7acc7702e4c5fa4107e3b907c7
namn44241 <nguyenvandoaaaaa@gmail.com>
2024-12-19 00:53:12 +0700

    [ADD] App android, ép lanscape

--- react/client/android/app/src/main/java/com/foodee/admin/MainActivity.java
+++ react/client/android/app/src/main/java/com/foodee/admin/MainActivity.java
@@ -1,5 +1,13 @@
 package com.foodee.admin;
 
 import com.getcapacitor.BridgeActivity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
